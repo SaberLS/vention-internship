3a0d33e36caf3e2542112b21d7db0bd844eca358
SaberLS <Patryk.Platek@ventionteams.com>
2026-05-29 22:57:46 +0200

    implement Rocket class

---
+++ TASKS/OOP internship task/src/SpaceMissionComponents/Rocket.mjs
@@ -0,0 +1,55 @@
+class Rocket {
+  #fuelLevel = 100;
+  name;
+  payloadCapacityKg;
+  reusable;
+
+  /**
+   *
+   * @param {string} name
+   * @param {number} payloadCapacityKg
+   * @param {boolean} reusable
+   */
+  constructor(name, payloadCapacityKg, reusable) {
+    this.name = name;
+    this.payloadCapacityKg = payloadCapacityKg;
+    this.reusable = reusable;
+  }
+
+  /**
+   * @returns {void}
+   */
+  refuel() {
+    this.#fuelLevel = 100;
+  }
+
+  /**
+   *
+   * @param {number} amount
+   * @returns {void | never}
+   */
+  consumeFuel(amount) {
+    this.#fuelLevel -= amount;
+
+    if (this.#fuelLevel < 0)
+      throw new Error(`Fuel below critical level ${this.#fuelLevel}`);
+  }
+
+  /**
+   *
+   * @returns {boolean}
+   */
+  isReadyToLaunch() {
+    return this.#fuelLevel >= 80;
+  }
+
+  /**
+   *
+   * @returns {string}
+   */
+  toString() {
+    return `🚀 ${this.name} | Capacity: ${this.payloadCapacityKg}kg | Reusable: ${this.reusable ? "Yes" : "No"} | Fuel: ${this.#fuelLevel}%`;
+  }
+}
+
+export { Rocket };
