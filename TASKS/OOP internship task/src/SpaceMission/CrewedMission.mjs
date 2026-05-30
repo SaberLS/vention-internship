dc1209a63cfc53b9ba7b58575938316a96578995
SaberLS <Patryk.Platek@ventionteams.com>
2026-05-30 17:08:46 +0200

    implement CrewedMission class

---
+++ TASKS/OOP internship task/src/SpaceMission/CrewedMission.mjs
@@ -0,0 +1,30 @@
+import { Mission } from "./Mission.mjs";
+
+class CrewedMission extends Mission {
+  crew;
+
+  /**
+   *
+   * @param {import('../SpaceMissionComponents/Crew.mjs').Crew} crew
+   * @param {string} name
+   * @param {import('../SpaceMissionComponents/Rocket.mjs').Rocket} rocket
+   * @param {number} payloadKg
+   */
+  constructor(name, rocket, payloadKgcrew, crew) {
+    super(name, rocket, payloadKgcrew);
+
+    this.crew = crew;
+  }
+
+  validate() {
+    super.validate();
+
+    if (!this.crew.isReady()) throw new Error("Crew is not ready!");
+  }
+
+  toString() {
+    return super.toString() + ` | Crew: ${this.crew.getSize()}`;
+  }
+}
+
+export { CrewedMission };
