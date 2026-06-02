3b50604bb49f330f666df09f7ec8c2abfc2bc7f4
SaberLS <patryk.platek19@gmail.com>
2026-06-02 14:37:28 +0200

    add sidebar

--- TASKS/html/vention-html-task/src/main.js
+++ TASKS/html/vention-html-task/src/main.js
@@ -1,1 +1,12 @@
+import { mountSidebar } from "./scripts/sidebar";
 
+const menuToggle = document.getElementById("menu-toggle");
+const toggleIcon = document.getElementById("toggle-icon").querySelector("use");
+const sideMenu = document.getElementById("side-menu");
+
+const icons = {
+  open: "./src/assets/svg-spreedsheet.svg#burger-menu",
+  close: "./src/assets/svg-spreedsheet.svg#close-menu",
+};
+
+mountSidebar({ menuToggle, toggleIcon, sideMenu }, icons);
