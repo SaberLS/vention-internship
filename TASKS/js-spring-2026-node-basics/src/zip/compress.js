d4206529c8cb7b37dd158dcf8bd5a70854681a46
SaberLS <patryk.platek19@gmail.com>
2026-06-13 16:57:09 +0200

    solve zip tasks

--- TASKS/js-spring-2026-node-basics/src/zip/compress.js
+++ TASKS/js-spring-2026-node-basics/src/zip/compress.js
@@ -1,5 +1,19 @@
+// - `compress.js` - implement function that compresses file `fileToCompress.txt` to `archive.gz` using `zlib` and Streams API
+
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+import { join } from "node:path";
+
 const compress = async () => {
-  // Write your code here
+  const gzip = createGzip();
+
+  const filesDir = join(import.meta.dirname, "files");
+
+  const source = createReadStream(join(filesDir, "fileToCompress.txt"));
+  const destination = createWriteStream(join(filesDir, "archive.gz"));
+
+  await pipeline(source, gzip, destination);
 };
 
 await compress();
--- TASKS/js-spring-2026-node-basics/src/zip/decompress.js
+++ TASKS/js-spring-2026-node-basics/src/zip/decompress.js
@@ -1,5 +1,17 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGunzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+import { join } from "node:path";
+
 const decompress = async () => {
-  // Write your code here
+  const gunzip = createGunzip();
+
+  const filesDir = join(import.meta.dirname, "files");
+
+  const source = createReadStream(join(filesDir, "archive.gz"));
+  const destination = createWriteStream(join(filesDir, "fileToCompress.txt"));
+
+  await pipeline(source, gunzip, destination);
 };
 
 await decompress();
