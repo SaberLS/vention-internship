8d9e3ec2d30e9bffd73eea76b6c2d0259a61881d
SaberLS <patryk.platek19@gmail.com>
2026-06-13 16:54:03 +0200

    solve cp task

--- TASKS/js-spring-2026-node-basics/src/cp/cp.js
+++ TASKS/js-spring-2026-node-basics/src/cp/cp.js
@@ -1,6 +1,28 @@
-const spawnChildProcess = async (args) => {
-  // Write your code here
-};
+import { spawn } from "node:child_process";
+import process from "node:process";
+import path from "node:path";
+
+const spawnChildProcess = async (args) =>
+  new Promise((resolve, reject) => {
+    const file = path.join(import.meta.dirname, "files", "script.js");
+    const child = spawn("node", [file, ...args]);
+
+    const onError = (error) => {
+      child.off("close", onClose);
+      reject(error);
+    };
+    const onClose = (data) => {
+      child.off("error", onError);
+      process.stdin.unpipe(child.stdin);
+      resolve(data);
+    };
+
+    child.once("error", onError);
+    child.once("close", onClose);
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+  });
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["test", "CLOSE"]);
