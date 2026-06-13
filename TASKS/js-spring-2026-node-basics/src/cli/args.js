65c2053f88d383e92bbec7638ff009c187723030
SaberLS <patryk.platek19@gmail.com>
2026-06-13 16:53:36 +0200

    solve cli tasks

--- TASKS/js-spring-2026-node-basics/src/cli/args.js
+++ TASKS/js-spring-2026-node-basics/src/cli/args.js
@@ -1,5 +1,21 @@
-const parseArgs = () => {
-  // Write your code here
-}
+import process from "node:process";
 
-parseArgs()
+const propName = /^--(.+)$/;
+const parseArgs = () => {
+  const args = process.argv.slice(2);
+  const result = {};
+
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i].slice(2);
+
+    if (key !== undefined) result[key] = args[i + 1];
+  }
+
+  process.stdout.write(
+    Object.entries(result)
+      .map((entry) => entry.join(" is "))
+      .join(", ") + "\n",
+  );
+};
+
+parseArgs();
--- TASKS/js-spring-2026-node-basics/src/cli/env.js
+++ TASKS/js-spring-2026-node-basics/src/cli/env.js
@@ -1,5 +1,14 @@
+import process from "node:process";
+
+const regex = /^TEST_/;
+
 const parseEnv = () => {
-  // Write your code here
+  const output = [];
+
+  for (const [key, value] of Object.entries(process.env))
+    if (regex.test(key)) output.push(`${key}=${value}`);
+
+  process.stdout.write(output.join("; ") + "\n");
 };
 
 parseEnv();
